bfaa72fd7ec900b29c4b4d5d529995cd8d2aed1c
Kat R. <153114658+katrazo@users.noreply.github.com>
2024-10-15 15:13:45 -0400

    Add dependencies SLF4J (logging), JsonPath (JSON Parsing), IOUtils (Data Handling)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,15 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-nop
+    implementation("org.slf4j:slf4j-nop:2.0.16")
+
+    // https://mvnrepository.com/artifact/com.jayway.jsonpath/json-path
+    implementation("com.jayway.jsonpath:json-path:2.9.0")
+
+    // https://mvnrepository.com/artifact/commons-io/commons-io
+    implementation("commons-io:commons-io:2.17.0")
 }
 
 tasks.test {
