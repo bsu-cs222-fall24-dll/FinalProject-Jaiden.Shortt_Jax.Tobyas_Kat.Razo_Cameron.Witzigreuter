5d0a5b6fd7c675e395691685251532eb3fff796a
Cameron311 <cwitz17@gamil.com>
2024-10-20 21:14:21 -0400

    Add DataStorageTest and DataStorage

--- src/main/java/edu/bsu/cs/DataStorage.java
+++ src/main/java/edu/bsu/cs/DataStorage.java
@@ -1,5 +1,30 @@
 package edu.bsu.cs;
+import java.util.List;
 
 public class DataStorage {
 
+    private final String redirect;
+    private final List<String> users;
+    private final List<String> times;
+    private final List<String> dates;
+
+    protected DataStorage(String redirect, List<String> users, List<String> times, List<String> dates) {
+
+        this.redirect= redirect;
+        this.users = users;
+        this.times = times;
+        this.dates = dates;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        if (!redirect.isEmpty())
+            stringBuilder.append(String.format("Redirected to %s%n", redirect));
+
+        for (int i = 0; i < users.size(); i++){
+            stringBuilder.append(String.format("%s  %s  %s%n", users.get(i), times.get(i), dates.get(i)));
+        }
+        return stringBuilder.toString();
+    }
 }
