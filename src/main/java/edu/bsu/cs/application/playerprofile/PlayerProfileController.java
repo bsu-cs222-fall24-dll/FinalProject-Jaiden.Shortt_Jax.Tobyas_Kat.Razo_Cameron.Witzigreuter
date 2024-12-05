877cae9f1da8dc72f15a4695054ff5139a5f50f3
Cameron311 <cwitz17@gamil.com>
2024-12-05 18:14:07 -0500

    Update playerprofile-view.fxml, make weblink a hyperlink in PlayerProfileController

--- src/main/java/edu/bsu/cs/application/playerprofile/PlayerProfileController.java
+++ src/main/java/edu/bsu/cs/application/playerprofile/PlayerProfileController.java
@@ -1,13 +1,19 @@
 package edu.bsu.cs.application.playerprofile;
 
 import edu.bsu.cs.records.PlayerStorage;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
 
 public class PlayerProfileController {
     @FXML private Label userNameDisplayLabel;
     @FXML private Label typeDisplayLabel;
-    @FXML private Label weblinkDisplayLabel;
+    @FXML private Hyperlink weblinkHyperlink;
     @FXML private Label twitchDisplayLabel;
     @FXML private Label hitBoxDisplayLabel;
     @FXML private Label youtubeDisplayLabel;
@@ -16,13 +22,16 @@ public class PlayerProfileController {
 
     public void inputPlayerInformation(PlayerStorage playerStorage){
         userNameDisplayLabel.setText(playerStorage.name());
-        weblinkDisplayLabel.setText(playerStorage.weblink());
+        weblinkHyperlink.setText(playerStorage.weblink());
         typeDisplayLabel.setText(playerStorage.type());
         twitchDisplayLabel.setText(playerStorage.socialMedias().get("twitch"));
         hitBoxDisplayLabel.setText(playerStorage.socialMedias().get("hitbox"));
         youtubeDisplayLabel.setText(playerStorage.socialMedias().get("youtube"));
         twitterDisplayLabel.setText(playerStorage.socialMedias().get("twitter"));
         speedrunsLiveDisplayLabel.setText(playerStorage.socialMedias().get("speedrunsLive"));
+    }
 
+    public void openLinkAction(ActionEvent actionEvent) throws IOException {
+        Desktop.getDesktop().browse(URI.create(((Hyperlink) actionEvent.getSource()).getText()));
     }
 }
